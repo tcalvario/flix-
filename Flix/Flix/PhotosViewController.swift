9308361c028bfbb6fd6dd81dbabec772c1dbdf69
tcalvario <tcalvari@mail.sfsu.edu>
2019-02-06 13:23:32 -0800

    flix Part 1 Submission

---
+++ Flix/Flix/PhotosViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PhotosViewController.swift
+//  Flix
+//
+//  Created by Teodoro Calvario on 2/6/19.
+//  Copyright © 2019 Teodoro Calvario. All rights reserved.
+//
+
+import UIKit
+
+class PhotosViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Flix/Flix/movieCell.swift
@@ -0,0 +1,24 @@
+//
+//  movieCell.swift
+//  Flix
+//
+//  Created by Teodoro Calvario on 2/6/19.
+//  Copyright © 2019 Teodoro Calvario. All rights reserved.
+//
+
+import UIKit
+
+class movieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
