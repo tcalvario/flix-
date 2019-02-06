9ad99fcc37a1b76e5b9f49bda8baa97a02854ec9
tcalvario <tcalvari@mail.sfsu.edu>
2019-02-06 09:58:52 -0800

    Flix inital setup

---
+++ Flix/Flix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flix
+//
+//  Created by Teodoro Calvario on 2/6/19.
+//  Copyright © 2019 Teodoro Calvario. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
